9f1791c26a3ba7369a7050138ae296a958726e8d
peterjc <p.j.a.cock@googlemail.com>
2013-11-28 15:01:24 +0000

    How to regenerate/update more test files

--- tools/ncbi_blast_plus/update_test_files.sh
+++ tools/ncbi_blast_plus/update_test_files.sh
@@ -11,11 +11,28 @@ fi
 
 cd test-data
 
+export EXT="6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen"
+
+echo
+echo makeblastdb
+echo ===========
+#Doing this first in case future tests use one of the local databases
+
+echo "four_human_proteins.fasta"
+makeblastdb -out four_human_proteins.fasta -hash_index -in four_human_proteins.fasta  -title "Just 4 human proteins" -dbtype prot
+
+echo
+echo Main
+echo ====
+
 echo "blastn_rhodopsin_vs_three_human.tabular"
 blastn -query rhodopsin_nucs.fasta -subject three_human_mRNA.fasta -task megablast -evalue 1e-40 -out blastn_rhodopsin_vs_three_human.tabular -outfmt 6
 
 echo "blastp_four_human_vs_rhodopsin.xml"
 blastp -query four_human_proteins.fasta -subject rhodopsin_proteins.fasta -task blastp -evalue 1e-08 -out blastp_four_human_vs_rhodopsin.xml -outfmt 5 -seg no -matrix BLOSUM62 -parse_deflines
+
+echo "blastp_four_human_vs_rhodopsin_ext.tabular"
+blastp -query four_human_proteins.fasta -subject rhodopsin_proteins.fasta -task blastp -evalue 1e-08 -out blastp_four_human_vs_rhodopsin_ext.tabular -outfmt "$EXT" -seg no -matrix BLOSUM62 -parse_deflines
 
 echo "blastx_rhodopsin_vs_four_human.xml"
 blastx -query rhodopsin_nucs.fasta -subject four_human_proteins.fasta -query_gencode 1 -evalue 1e-10 -out blastx_rhodopsin_vs_four_human.xml -outfmt 5
@@ -24,7 +41,7 @@ echo "blastx_rhodopsin_vs_four_human.tabular"
 blastx -query rhodopsin_nucs.fasta -subject four_human_proteins.fasta -query_gencode 1 -evalue 1e-10 -out blastx_rhodopsin_vs_four_human.tabular -outfmt 6
 
 echo "blastx_rhodopsin_vs_four_human_ext.tabular"
-blastx -query rhodopsin_nucs.fasta -subject four_human_proteins.fasta -query_gencode 1 -evalue 1e-10 -out blastx_rhodopsin_vs_four_human_ext.tabular -outfmt "6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen"
+blastx -query rhodopsin_nucs.fasta -subject four_human_proteins.fasta -query_gencode 1 -evalue 1e-10 -out blastx_rhodopsin_vs_four_human_ext.tabular -outfmt "$EXT"
 
 echo "tblastn_four_human_vs_rhodopsin.xml"
 tblastn -query four_human_proteins.fasta -subject rhodopsin_nucs.fasta -evalue 1e-10 -out tblastn_four_human_vs_rhodopsin.xml -outfmt 5 -db_gencode 1 -seg no -matrix BLOSUM80
@@ -32,11 +49,43 @@ tblastn -query four_human_proteins.fasta -subject rhodopsin_nucs.fasta -evalue 1
 echo "tblastn_four_human_vs_rhodopsin.html"
 tblastn -query four_human_proteins.fasta -subject rhodopsin_nucs.fasta -evalue 1e-10 -out tblastn_four_human_vs_rhodopsin.html -outfmt 0 -html -db_gencode 1 -seg no -matrix BLOSUM80
 
-echo "blastx_rhodopsin_vs_four_human_converted.tabular (via our script)"
-python /mnt/galaxy/galaxy-central/tools/ncbi_blast_plus/blastxml_to_tabular.py blastx_rhodopsin_vs_four_human.xml blastx_rhodopsin_vs_four_human_converted.tabular std
+echo "tblastn_four_human_vs_rhodopsin_ext.tabular"
+tblastn -query four_human_proteins.fasta -subject rhodopsin_nucs.fasta -evalue 1e-10 -out tblastn_four_human_vs_rhodopsin_ext.tabular -outfmt "$EXT" -db_gencode 1 -seg no -matrix BLOSUM80
 
-echo "blastx_rhodopsin_vs_four_human_converted_ext.tabular (via our script)"
-python /mnt/galaxy/galaxy-central/tools/ncbi_blast_plus/blastxml_to_tabular.py blastx_rhodopsin_vs_four_human.xml blastx_rhodopsin_vs_four_human_converted_ext.tabular ext
+echo "tblastx_rhodopsin_vs_three_human.tabular"
+tblastx -query rhodopsin_nucs.fasta -subject three_human_mRNA.fasta -evalue 1e-40 -out tblastx_rhodopsin_vs_three_human.tabular -outfmt 6
+
+echo
+echo blastxml_to_tabular
+echo ===================
+
+echo "blastp_four_human_vs_rhodopsin_converted.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastp_four_human_vs_rhodopsin.xml blastp_four_human_vs_rhodopsin_converted.tabular std
+
+echo "blastp_four_human_vs_rhodopsin_converted_ext.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastp_four_human_vs_rhodopsin.xml blastp_four_human_vs_rhodopsin_converted_ext.tabular ext
+
+echo "blastp_sample_converted.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastp_sample.xml blastp_sample_converted.tabular std
+
+echo "blastx_rhodopsin_vs_four_human_converted.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastx_rhodopsin_vs_four_human.xml blastx_rhodopsin_vs_four_human_converted.tabular std
+
+echo "blastx_rhodopsin_vs_four_human_converted_ext.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastx_rhodopsin_vs_four_human.xml blastx_rhodopsin_vs_four_human_converted_ext.tabular ext
+
+echo "blastx_sample_converted.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastx_sample.xml blastx_sample_converted.tabular std
+
+echo "blastp_human_vs_pdb_seg_no_converted_std.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastp_human_vs_pdb_seg_no.xml blastp_human_vs_pdb_seg_no_converted_std.tabular std
+
+echo "blastp_human_vs_pdb_seg_no_converted_ext.tabular"
+python ../tools/ncbi_blast_plus/blastxml_to_tabular.py blastp_human_vs_pdb_seg_no.xml blastp_human_vs_pdb_seg_no_converted_ext.tabular ext
+
+echo
+echo deltablast
+echo ==========
 
 #It will be a problem if the exact version of the cdd_delta database alters the test output...
 #Following (or similar) works for deltablast to find the cdd_delta database automatically:
@@ -51,7 +100,7 @@ echo "deltablast_four_human_vs_rhodopsin.tabular"
 deltablast -query four_human_proteins.fasta -subject rhodopsin_proteins.fasta -evalue 1e-08 -out deltablast_four_human_vs_rhodopsin.tabular -outfmt 6 -matrix BLOSUM62 -seg no -parse_deflines -rpsdb $CDD_DELTA
 
 echo "deltablast_four_human_vs_rhodopsin_ext.tabular"
-deltablast -query four_human_proteins.fasta -subject rhodopsin_proteins.fasta -evalue 1e-08 -out deltablast_four_human_vs_rhodopsin_ext.tabular -outfmt "6 std sallseqid score nident positive gaps ppos qframe sframe qseq sseq qlen slen" -matrix BLOSUM62 -seg no -parse_deflines -rpsdb $CDD_DELTA
+deltablast -query four_human_proteins.fasta -subject rhodopsin_proteins.fasta -evalue 1e-08 -out deltablast_four_human_vs_rhodopsin_ext.tabular -outfmt "$EXT" -matrix BLOSUM62 -seg no -parse_deflines -rpsdb $CDD_DELTA
 
 echo "deltablast_rhodopsin_vs_four_human.tabular"
 deltablast -query rhodopsin_proteins.fasta -subject four_human_proteins.fasta -evalue 1e-08 -out deltablast_rhodopsin_vs_four_human.tabular -outfmt 6 -rpsdb $CDD_DELTA
