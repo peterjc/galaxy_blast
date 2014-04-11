7a98be6fd8d9a2784eada30223b5c847c7077d83
Michael Li <michael.li@uwaterloo.ca>
2014-04-11 13:47:51 +0100

    Expand NCBI BLASTDB datamanager to cover protein and protein domains.
    Based on a branch by @mike8115, rebased with trivial changes
    by @peterjc. Does not (yet) use Data Tables (from_data_table)
    in the main tool wrappers.

--- data_managers/ncbi_blastdb/fetch_blast_db.py
+++ data_managers/ncbi_blastdb/fetch_blast_db.py
@@ -1,42 +1,16 @@
 #!/usr/bin/env python
-#Dan Blankenberg
-#Script that calls update_blastdb.pl to download preformatted databases
+#Adapted from Dan Blankenberg's data_manager_example_blastdb_ncbi_update_blastdb
+#Michael Li, Microbial Biodiversity Bioinformatics group, Agriculture and Agri-Food Canada, April 2014
+#Script that downloads preformatted databases from NCBI.
 
 import optparse
 import os
 import sys
 import subprocess
-import hashlib
-
+import time
+import tarfile
+from ftplib import FTP
 from galaxy.util.json import from_json_string, to_json_string
-DEFAULT_ALGORITHM = hashlib.sha512
-CHUNK_SIZE = 2**20 #1mb
-
-def get_dir_hash( directory, algorithm=None, followlinks=True, chunk_size=None ):
-    chunk_size = chunk_size or CHUNK_SIZE
-    algorithm = algorithm or DEFAULT_ALGORITHM
-    if isinstance( algorithm, basestring ):
-        hash = hashlib.new( algorithm )
-    else:
-        hash = algorithm()
-    #we hash a directory by taking names of directories, files and their contents
-    for dirpath, dirnames, filenames in os.walk( directory, followlinks=followlinks ):
-        dirnames.sort()
-        filenames.sort()
-        for name in dirnames:
-            hash.update( os.path.relpath( os.path.join( dirpath, name ), directory ) )
-        for name in filenames:
-            filename = os.path.join( dirpath, name )
-            hash.update( os.path.relpath( filename, directory ) )
-            fh = open( filename, 'rb' )
-            while True:
-                data = fh.read( chunk_size )
-                if not data:
-                    break
-                hash.update( data )
-            fh.close()
-    
-    return hash.hexdigest()
 
 def main():
     #Parse Command Line
@@ -45,44 +19,104 @@ def main():
     parser.add_option( '-t', '--tool_data_table_name', dest='tool_data_table_name', action='store', type='string', default=None, help='tool_data_table_name' )
     (options, args) = parser.parse_args()
     
+    #Take the JSON input file for parsing
     params = from_json_string( open( options.filename ).read() )
     target_directory = params[ 'output_data' ][0]['extra_files_path']
     os.mkdir( target_directory )
-    
-    blastdb_name = params['param_dict']['blastdb_name'] #value
+
+    #Fetch parameters from input JSON file
+    blastdb_name = params['param_dict']['db_type'].get( 'blastdb_name' )
+    blastdb_type = params['param_dict']['db_type'].get( 'blastdb_type' )
     data_description = params['param_dict']['advanced'].get( 'data_description', None )
     data_id = params['param_dict']['advanced'].get( 'data_id', None )
     
-    cmd_options = [ '--decompress' ]
-    
-    args = [ 'update_blastdb.pl' ] + cmd_options + [ blastdb_name ]
-    proc = subprocess.Popen( args=args, shell=False, cwd=target_directory )
-    return_code = proc.wait()
-    if return_code != 1:
-        print >> sys.stderr, "Error obtaining blastdb (%s)" % return_code
-        sys.exit( 1 )
-    
-    if not data_id:
-        data_id = "%s_%s" % ( blastdb_name, get_dir_hash( target_directory ) )
-    
-    if not data_description:
-        alias_date = None
+    #update_blastdb.pl doesn't download protein domains, so we use ftp
+    if blastdb_type == 'blastdb_d':
         try:
-            for line in open( os.path.join( target_directory, "%s.nal" % ( blastdb_name ) ) ):
-                if line.startswith( '# Alias file created ' ):
-                    alias_date = line.split( '# Alias file created ', 1 )[1].strip()
-                if line.startswith( 'TITLE' ):
-                    data_description = line.split( None, 1 )[1].strip()
-                    break
+            archive_name = blastdb_name + '_LE.tar.gz'
+            tar_file = open( os.path.join( target_directory, archive_name ), "wb" )
+
+            #Connect via ftp and download
+            ftp = FTP('ftp.ncbi.nih.gov')
+            ftp.login()
+            ftp.cwd('pub/mmdb/cdd/little_endian')
+            ftp.retrbinary('RETR %s' % archive_name, tar_file.write)
+            tar_file.close()
+
+            #Extract contents
+            tar_file = tarfile.open(os.path.join( target_directory, archive_name ), mode='r')
+            tar_file.extractall( target_directory )
+            tar_file.close()
+
+        #If the download fails, ftplib should generate an error in ftplib.all_errors
+        #Likewise, tarfile.ReadError should catch any errors when reading from the tar
+        #And other possible errors that can occur here...
+        except IOError, e:
+            print >> sys.stderr, "Cannot create file: %s: %s" % ( archive_name, e )
+            sys.exit( 1 )
+
+        except os.error, e:
+            print "Error while joining %s and %s: %s" % ( target_directory, archive_name, e )
+            sys.exit( 1 )
+
+        except ftplib.all_errors, e:
+            print >> sys.stderr, "Error while downloading protein domain database: %s" % ( e )
+            sys.exit( 1 )
+
+        except tarfile.TarError, e:
+            print >> sys.stderr, "Error while opening/extracting the tar file: %s" % ( e )
+            sys.exit( 1 )
+
+    else:
+        #Run update_blastdb.pl
+        cmd_options = [ '--decompress' ]
+        args = [ 'update_blastdb.pl' ] + cmd_options + [ blastdb_name ]
+        proc = subprocess.Popen( args=args, shell=False, cwd=target_directory )
+        return_code = proc.wait()
+
+        #Check if download was successful (exit code 1)
+        if return_code != 1:
+            print >> sys.stderr, "Error obtaining blastdb (%s)" % return_code
+            sys.exit( 1 )
+    
+    #Set id and description if not provided in the advanced settings
+    if not data_id:
+        #Use download time to create uniq id
+        localtime = time.localtime()
+        timeString = time.strftime("%Y_%m_%d", localtime)
+        data_id = "%s_%s" % ( blastdb_name, timeString )
+    
+    # Attempt to automatically set description from alias file
+    # Protein domain databases don't have an alias file
+    if not data_description and blastdb_type != 'blastdb_d':
+        alias_date = None
+        alias_file = None
+        try:
+            if blastdb_type == 'blastdb':
+                alias_file = "%s.nal" % ( blastdb_name )
+            if blastdb_type == 'blastdb_p':
+                alias_file = "%s.pal" % ( blastdb_name )
+            if alias_file:
+                for line in open( os.path.join( target_directory, alias_file ) ):
+                    if line.startswith( '# Alias file created ' ):
+                        alias_date = line.split( '# Alias file created ', 1 )[1].strip()
+                    if line.startswith( '# Date created: ' ):
+                        alias_date = line.split( '# Date created: ', 1)[1].strip()
+                    if line.startswith( 'TITLE' ):
+                        data_description = line.split( None, 1 )[1].strip()
+                        break
         except Exception, e:
             print >> sys.stderr, "Error Parsing Alias file for TITLE and date: %s" % ( e )
+        #If we manage to parse the pal or nal file, set description
         if alias_date and data_description:
             data_description = "%s (%s)" % ( data_description, alias_date )
     
+    #If we could not parse the nal or pal file for some reason
     if not data_description:
         data_description = data_id
     
-    data_table_entry = { 'value':data_id, 'name':data_description, 'path': os.path.join( blastdb_name, data_id ), 'nucleotide_alias_name': blastdb_name }
+    #Prepare output string to convert into JSON format
+    data_table_entry = { 'value':data_id, 'name':data_description, 'path': os.path.join( blastdb_name, data_id ), 'database_alias_name': blastdb_name }
     data_manager_dict = { 'data_tables': { options.tool_data_table_name: [ data_table_entry ]  } }
     
     #save info to json file
---
+++ data_managers/ncbi_blastdb/make_data_manager_blastdb.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+echo "This will create a tar-ball suitable to upload to the toolshed."
+
+if [ -f "data_managers/ncbi_blastdb/make_data_manager_blastdb.sh" ]
+then
+echo "Good, in the expected directory"
+else
+echo "ERROR. Run this from the GitHub repository root directory."
+exit 1
+fi
+
+if [ -f "ncbi_blastdb.tar.gz" ]
+then
+echo "ERROR. File ncbi_blastdb.tar.gz already exists."
+exit 1
+fi
+
+#Create tar file with core XML wrappers
+if [ -f "ncbi_blastdb.tar" ]
+then
+rm data_manager_blastdb.tar
+fi
+
+#Create tar file (-cf then -rf to add to it)
+tar -cf  ncbi_blastdb.tar data_managers/ncbi_blastdb/data_manager_conf.xml
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/README.rst
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/tool_dependencies.xml
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/blastdb.xml
+tar -rf  ncbi_blastdb.tar data_managers/ncbi_blastdb/fetch_blast_db.py
+tar -rf  ncbi_blastdb.tar tool-data/blastdb.loc.sample
+tar -rf  ncbi_blastdb.tar tool-data/blastdb_p.loc.sample
+tar -rf  ncbi_blastdb.tar tool-data/blastdb_d.loc.sample
+tar -rf  ncbi_blastdb.tar tool-data/tool_data_table_conf.xml.sample
+tar -rf  ncbi_blastdb.tar test-data/cog_out.json
+tar -rf  ncbi_blastdb.tar test-data/pataa_out.json
+tar -rf  ncbi_blastdb.tar test-data/patnt_out.json
+
+
+#Zip the tar file
+gzip ncbi_blastdb.tar
+
+#Check the output
+echo "Expect a tar-ball with 12 files, have:"
+tar -tzf ncbi_blastdb.tar.gz | wc -l
