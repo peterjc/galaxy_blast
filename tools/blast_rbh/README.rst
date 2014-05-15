e52ae683a7ee1bc616ab073523e1cbfbbc4f5765
peterjc <p.j.a.cock@googlemail.com>
2014-05-15 11:32:02 +0100

    Started work extending reciprocal_best_hits --> blast_rbh tool

---
+++ tools/blast_rbh/README.rst
@@ -0,0 +1,35 @@
+Galaxy tool to find BLAST Reciprocal Best Hits (RBH)
+====================================================
+
+This tool is copyright 2011-2014 by Peter Cock, The James Hutton Institute
+(formerly SCRI, Scottish Crop Research Institute), UK. All rights reserved.
+See the licence text below.
+
+This tool is a short Python script to run reciprocal BLAST searches on a
+pair of sequence files, and extract the reciprocal best hits.
+
+This is a work in progress, and builds on an earlier implementation which
+prequired the two BLAST searches be prepared in advance. Integration allows
+a much simpler user experience, and can ensure sensible filters are used.
+
+
+Licence (MIT)
+=============
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
---
+++ tools/blast_rbh/blast_rbh.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+"""BLAST Reciprocal Best Hit (RBH) from two FASTA input files.
+
+Takes the following command line options,
+1. FASTA filename of species A
+2. FASTA filename of species B
+3. BLAST type (e.g. blastn, or blastp) from which infer prot/nucl
+4. Minimum BLAST Percentage identity
+5. Minimum BLAST query coverage
+6. Output filename
+"""
+
+import os
+import sys
+
+if "--version" in sys.argv[1:]:
+    print "BLAST RBH v0.1.0"
+    sys.exit(0)
+
+def stop_err( msg ):
+    sys.stderr.write("%s\n" % msg)
+    sys.exit(1)
+
+#Parse Command Line
+#TODO - optparse
+try:
+    fasta_a, fasta_b, blast_type, min_identity, min_coverage, out_file = sys.argv[1:]
+except:
+    stop_err("Expect 6 arguments, got %i" % (len(sys.argv) - 1))
+
+if not os.path.isfile(fasta_a):
+    stop_err("Missing input file for species A: %r" % fasta_a)
+if not os.path.isfile(fasta_b):
+    stop_err("Missing input file for species B: %r" % fasta_b)
+
+try:
+    min_identity = float(min_identity)
+except ValueError:
+    stop_err("Expected number between 0 and 100 for minimum identity, not %r" % min_identity)
+if not (0 <= min_identity <= 100):
+    stop_err("Expected minimum identity between 0 and 100, not %0.2f" % min_identity)
+try:
+    min_coverage = float(min_coverage)
+except ValueError:
+    stop_err("Expected number between 0 and 100 for minimum coverage, not %r" % min_coverage)
+if not (0 <= min_coverage <= 100):
+    stop_err("Expected minimum coverage between 0 and 100, not %0.2f" % min_coverage)
+
+
+stop_err("Not implemented yet...")
+
+"""
+def load_best(filename, id1col, id2col):
+    best = dict()
+    for line in open(a_vs_b):
+        if line.startswith("#"): continue
+        parts = line.rstrip("\n").split("\t")
+        id1 = parts[id1col]
+        id2 = parts[id2col]
+        score = float(parts[c_score])
+        if (a not in best_a_vs_b) \
+        or (want_highest and score > best[id1][1]) \
+        or (want_lowest and score < best[id1][1]):
+            best[id1] = (id2, score)
+    return best
+best_a_vs_b = load_best(a_vs_b, c_query, c_match)
+"""
+
+best_a_vs_b = dict()
+for line in open(a_vs_b):
+    if line.startswith("#"): continue
+    parts = line.rstrip("\n").split("\t")
+    a = parts[c_query]
+    b = parts[c_match]
+    score = float(parts[c_score])
+    if (a not in best_a_vs_b) \
+    or (want_highest and score > best_a_vs_b[a][1]) \
+    or (want_lowest and score < best_a_vs_b[a][1]):
+        best_a_vs_b[a] = (b, score, parts[c_score])
+b_short_list = set(b for (b,score, score_str) in best_a_vs_b.values())
+
+best_b_vs_a = dict()
+for line in open(b_vs_a):
+    if line.startswith("#"): continue
+    parts = line.rstrip("\n").split("\t")
+    b = parts[c_query]
+    a = parts[c_match]
+    if a not in best_a_vs_b:
+        continue
+        #stop_err("The A-vs-B file does not have A-ID %r found in B-vs-A file" % a)
+    if b not in b_short_list: continue
+    score = float(parts[c_score])
+    if (b not in best_b_vs_a) \
+    or (want_highest and score > best_b_vs_a[b][1]) \
+    or (want_lowest and score < best_b_vs_a[b][1]):
+        best_b_vs_a[b] = (a, score, parts[c_score])
+#TODO - Preserve order from A vs B?
+a_short_list = sorted(set(a for (a,score,score_str) in best_b_vs_a.values()))
+
+count = 0
+outfile = open(out_file, 'w')
+outfile.write("#A_id\tB_id\tA_vs_B\tB_vs_A\n")
+for a in a_short_list:
+    b = best_a_vs_b[a][0]
+    if b in best_b_vs_a and a == best_b_vs_a[b][0]:
+        outfile.write("%s\t%s\t%s\t%s\n" % (a, b, best_a_vs_b[a][2], best_b_vs_a[b][2]))
+        count += 1
+outfile.close()
+print "Done, %i RBH found" % count
