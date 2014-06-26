5b1fa8dd8b74e7b007f6ed2b5a7691c873188b50
Jan Kanis <jan.code@jankanis.nl>
2014-06-26 12:57:31 +0200

    add -q option to limit_query_size.py; refactor

--- tools/ncbi_blast_plus/limit_query_size.py
+++ tools/ncbi_blast_plus/limit_query_size.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
 """
-This program checks if the size of a query file exceeds a configured maximum size. 
+limit_query_size.py [-q] querysize_max command... -query file ...
+
+This program checks if the size of a query file exceeds a configured
+maximum size.
 
 Usage example:
 limit_query_size.py "$GALAXY_BLAST_QUERYSIZE_MAX" blastn -query "$query" ...other blast options...
 
-The GALAXY_BLAST_QUERYSIZE_MAX parameter can be a number, optionally suffixed by K, M or G for kibibytes, mebibites or gibibytes.
-If the parameter is empty the command will be run normally.
+The querysize_max parameter can be a number, optionally suffixed by K,
+M or G for kibibytes, mebibites or gibibytes. This program will search
+for a '-query' option with a file argument, and verify that the file
+is no larger than querysize_max. If so, limit_querysize.py will
+execute the command, else it will print an error and not execute the
+command. If the querysize_max parameter is empty the command will be
+run normally.
 """
 
 from __future__ import print_function, unicode_literals
@@ -16,24 +24,30 @@ from __future__ import print_function, unicode_literals
 import sys
 import os.path
 import re
+import optparse
+from optparse import OptionParser
 
 
-self = querymax = command = None
+self = querymax = command = quiet = None
 
 
 def parse_cmdline():
-    global self, querymax, command
+    global self, querymax, command, quiet
 
-    if len(sys.argv) > 1 and sys.argv[1] in ('-h', '--help'):
-        print(__doc__)
-        exit(0)
-    if len(sys.argv) < 3:
-        print(__doc__)
+    parser = OptionParser(usage=__doc__.rstrip())
+    parser.add_option('-q', '--quiet', dest='quiet', action='count', default=0,
+                      help="Don't print warning messages. Specify twice to also disable printing error messages.")
+    parser.disable_interspersed_args()
+    (options, args) = parser.parse_args()
+
+    if len(args) < 2:
+        parser.print_help()
         exit(1)
 
     self = os.path.basename(sys.argv[0])
-    querymax = sys.argv[1]
-    command = sys.argv[2:]
+    querymax = args[0]
+    command = args[1:]
+    quiet = options.quiet
     
 
 def main():
@@ -41,9 +55,9 @@ def main():
     if not querymax:
         exec_command()
 
-    match = re.match(r'^(\d+)([KMG]?)i?$', querymax)
+    match = re.match(r'^(\d+)([KMG]?)(?:iB?)?$', querymax)
     if match is None:
-        print("%s: Warning: Error in query limit format: %s" % (self, querymax), file=sys.stderr)
+        warn("Error in query limit format: %s" % querymax)
         exec_command()
     
     value, suffix = match.groups()
@@ -53,8 +67,17 @@ def main():
 
     if check_query_file_size(value):
         exec_command()
+        # exec_command does not return
+    else:
+        exit(2)
 
-    return 2
+def warn(str):
+    if quiet < 1:
+        print("%s: Warning: %s" % (self, str), file=sys.stderr)
+
+def err(str):
+    if quiet < 2:
+        print("%s: Error: %s" % (self, str), file=sys.stderr)
     
 
 def check_query_file_size(max_length):
@@ -62,16 +85,16 @@ def check_query_file_size(max_length):
         i = command.index('-query')
         path = command[i+1]
     except (ValueError, IndexError):
-        print("%s: Warning: No '-query' file path found in command" % self)
+        warn("No '-query' file path found in command")
         return True
     try:
         filesize = os.path.getsize(path)
         if filesize > max_length:
-            print("%s: Error: query is larger than %s bytes" % (self, querymax), file=sys.stderr)
+            err("query size too large (size %s bytes, max %s)" % (filesize, querymax))
             return False
         return True
     except OSError:
-        print("%s: Warning: query file '%s' does not exist or is unreadable" % (self, path), file=sys.stderr)
+        warn("query file '%s' does not exist or is unreadable" % path)
         return True
             
 
@@ -79,7 +102,7 @@ def exec_command():
     try:
         os.execvp(command[0], command)
     except OSError as e:
-        print("%s: Error: %s: %s" % (self, command[0], str(e)), file=sys.stderr)
+        err("%s: %s" % (command[0], str(e)))
         exit(1)
 
 
